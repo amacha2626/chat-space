f6a955fe6280e522b10e3479160d788ee06babea
amacha2626 <amacha2626@gmail.com>
2020-03-04 11:51:22 +0900

    add ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,62 @@
+$(function(){
+
+  function buildHTML(message){
+    if (message.image){
+      var html = `<div class="chat-main__message-list__message-container__message">
+                    <div class="chat-main__message-list__message-container__message__message-upper">
+                      <p class="chat-main__message-list__message-container__message__message-upper__name">
+                        ${message.user_name}
+                      </p>
+                      <p class="chat-main__message-list__message-container__message__message-upper__update-time">
+                        ${message.created_at}
+                      </p>
+                    </div>
+                    <p class="chat-main__message-list__message-container__message__text">
+                      ${message.content}<br>
+                      <img src=${message.image}>
+                    </p>
+                  </div>`
+                  return html;
+    } else {
+      var html = `<div class="chat-main__message-list__message-container__message">
+                    <div class="chat-main__message-list__message-container__message__message-upper">
+                      <p class="chat-main__message-list__message-container__message__message-upper__name">
+                        ${message.user_name}
+                      </p>
+                      <p class="chat-main__message-list__message-container__message__message-upper__update-time">
+                        ${message.created_at}
+                      </p>
+                    </div>
+                    <p class="chat-main__message-list__message-container__message__text">
+                        ${message.content}
+                    </p>
+                  </div>`
+                  return html;
+    };
+  }
+
+  $("#new_message").on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__message-list__message-container').append(html);
+      $('#new_message')[0].reset();
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $('.chat-main__message-form__send').prop('disabled', false);
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+      $('.chat-main__message-form__send').prop('disabled', false);
+    })
+  })
+});
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
   def index
+    # @group = Group.find(params[:group_id])
+    # @users = @group.users
   end
   
   def new
