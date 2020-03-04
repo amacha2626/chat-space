ae679fb286d61f6322b4ea9736fbed7c62e309a2
amacha2626 <amacha2626@gmail.com>
2020-03-04 17:36:07 +0900

    add auto update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
 
   function buildHTML(message){
     if (message.image){
-      var html = `<div class="chat-main__message-list__message-container__message">
+      var html = `<div class="chat-main__message-list__message-container__message", data-message-id="${message.id}">
                     <div class="chat-main__message-list__message-container__message__message-upper">
                       <p class="chat-main__message-list__message-container__message__message-upper__name">
                         ${message.user_name}
@@ -18,7 +18,7 @@ $(function(){
                   </div>`
                   return html;
     } else {
-      var html = `<div class="chat-main__message-list__message-container__message">
+      var html = `<div class="chat-main__message-list__message-container__message", data-message-id="${message.id}">
                     <div class="chat-main__message-list__message-container__message__message-upper">
                       <p class="chat-main__message-list__message-container__message__message-upper__name">
                         ${message.user_name}
@@ -59,4 +59,30 @@ $(function(){
       $('.chat-main__message-form__send').prop('disabled', false);
     })
   })
+
+  var reloadMessages = function(){
+    var last_message_id = $('.chat-main__message-list__message-container__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: "json",
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+          insertHTML += buildHTML(message)
+        });
+        $(".chat-main__message-list__message-container").append(insertHTML);
+        $(".chat-main__message-list").animate({ scrollTop: $(".chat-main__message-list__message-container")[0].scrollHeight });
+      }
+    })
+    .fail(function(){
+      alert("error");
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 7000);
+  }
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,4 @@ json.content @message.content
 json.image @message.image.url
 json.user_name @message.user.name
 json.created_at @message.created_at.strftime("%Y年%m月5d日 %H時%M分")
+json.id @message.id
