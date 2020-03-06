456c4d9c7769ec0f0e33c4217bad793c0cad0c42
amacha2626 <amacha2626@gmail.com>
2020-03-06 12:02:16 +0900

    add validattion

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_many :messages
 
-  validates :name, presence: true
+  validates :name, presence: true,uniqueness: true
 
   def self.search(input, id)
     return nil if input == ""
